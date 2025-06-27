43a4f8e8d5d8550effe99a3980f6bac28e1c8e65
Rafael Iagher <rafaeliaghercontato@gmail.com>
2025-06-27 06:12:35 -0700

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
       {/* React Fragment <> */}
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Ola mundo 1
+      </Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
         assumenda minus, pariatur deserunt eveniet nam sit quas, dolore in quos
