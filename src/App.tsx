aa82e9d4a049b6bef000423ceea0abf7e5d0cc24
Rafael Iagher <rafaeliaghercontato@gmail.com>
2025-06-28 04:54:32 -0700

    Organizando componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure
-              libero debitis sed possimus. Eos officia blanditiis alias maxime
-              quo! Vel, alias nihil. Non autem, sint adipisci saepe dignissimos
-              corporis ea?
-            </p>
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
     </>
   );
 }
