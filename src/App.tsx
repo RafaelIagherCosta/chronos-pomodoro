ae98c83d94565e6cfc66e1401cbb386b75703d07
Rafael Iagher <fael.iagher28082007@gmail.com>
2025-06-30 14:48:11 -0700

    Limpando o código + Tema do VSCode

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -12,28 +11,10 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { useState } from 'react';
 
 export function App() {
-  //Quero que todos os componentes que usam "numero"
-  // saimbam das mudancas em seu valor.
-
-  //Sempre que eu usar useState, nao vou usar atribuicao diretamente regra!
-  //const [numero, setNumero] = useState((0) => {
-  // console.log('Lazy initialization)
-  //return 0
-  //});
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    setNumero(prevState => prevState + 1);
-    setNumero(1);
-  }
-
   return (
     <>
-      <Heading>Add commentMore actions Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -47,7 +28,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task'
               id='meuInput'
               type='text'
               placeholder='Ex: estudar para a prova'
